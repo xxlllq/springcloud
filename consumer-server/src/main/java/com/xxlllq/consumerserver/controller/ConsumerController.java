7d332cc8ada6ee472b99685b160b69d2e3f3f8bb
xiangxl <1101464221@qq.com>
2018-12-18 16:59:58 +0800

    负载均衡Demo

--- consumer-server/src/main/java/com/xxlllq/consumerserver/controller/ConsumerController.java
+++ consumer-server/src/main/java/com/xxlllq/consumerserver/controller/ConsumerController.java
@@ -4,16 +4,23 @@ import com.xxlllq.consumerserver.remote.ProducerRemote;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ConsumerController {
 
+    //    @Autowired
+//    private ProducerRemote producerRemote;
+//
+//    @GetMapping("/")
+//    public String get() {
+//        return "Consumer: " + producerRemote.get();
+//    }
     @Autowired
-    private ProducerRemote producerRemote;
+    private RestTemplate restTemplate;
 
-    @GetMapping("/")
-    public String get() {
-        return "Consumer: " + producerRemote.get();
+    @GetMapping(value = "/hello")
+    public String hello() {
+        return restTemplate.getForEntity("http://producer-server/", String.class).getBody();
     }
-
 }
